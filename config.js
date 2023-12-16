34f22e4f8d2d1269ec3634b7ade6fcd3024e23b2
nhatnhat1478520 <136719789+nhatnhat1478520@users.noreply.github.com>
2023-12-16 09:58:47 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Làm người yêu anh nhé Babe ! 😙',
+    desc: 'Em có đồng ý yêu anh không  ',
+    btnYes: 'Em đồng ý yêu anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Mãi mãi làm người yêu anh nha bé cưng.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://t.me/nhatnhat678' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
